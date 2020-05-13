f7f93e683b524ecdf891e5e8c412c8031345ea49
Iana.Kasimova <Smurfik55>
2020-05-13 15:24:45 +0300

    add gh actions

--- src/main/java/com/example/simplyCook/CookApplication.java
+++ src/main/java/com/example/simplyCook/CookApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.simplyCook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class CookApplication {
 
 	@RequestMapping("/")
 	public String home(){
@@ -15,7 +15,7 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CookApplication.class, args);
 	}
 
 }
